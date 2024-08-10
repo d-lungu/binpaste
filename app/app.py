d6dd3f16c30deac693f1bd931935508fc4da2e01
Denny Lungu <lungu.denny0@gmail.com>
2024-08-10 21:09:05 +0200

    Implemented Flask-Caching using Redis/KeyDB

--- app/app.py
+++ app/app.py
@@ -4,17 +4,29 @@ import pymongo
 import random
 import redis
 import flask_caching
+import flask_limiter
+
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
 
 app = flask.Flask(__name__)
 app.config.from_mapping(
     {
         "CACHE_TYPE": "RedisCache",
-        "CACHE_REDIS_HOST": redis.Redis(host="redis", port=6379),
+        "CACHE_REDIS_HOST": redis.Redis(host=REDIS_HOST, port=REDIS_PORT),
         "CACHE_DEFAULT_TIMEOUT": 60 * 60 * 24 * 30  # 1 month
     }
 )
 cache = flask_caching.Cache(app)
 PASTE_ID_LENGTH = 20
+limiter = flask_limiter.Limiter(
+    flask_limiter.util.get_remote_address,
+    app=app,
+    default_limits=["10 per second", "100 per minute", "250 per hour", "500 per day"],
+    # Redis
+    storage_uri="redis://localhost:6379",
+    strategy="fixed-window",  # or "moving-window"
+)
 
 
 def get_db() -> pymongo.MongoClient:
@@ -35,6 +47,11 @@ def generate_paste_id() -> str:
 @app.errorhandler(400)
 def resource_not_found(e):
     return flask.jsonify(error=str(e)), 400
+
+
+@app.errorhandler(429)
+def resource_not_found(e):
+    return flask.jsonify(error="429 Too Many Requests"), 429
 
 
 def get_config() -> dict:
