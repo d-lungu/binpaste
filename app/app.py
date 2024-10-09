52c93ac27fb48b8b71d0e52e5e4a7c032fb05eb5
Denny Lungu <lungu.denny0@gmail.com>
2024-10-09 11:36:45 +0200

    Replaced Redis cache with file system cache and MongoDB; updated Python to version 3.13.0

--- app/app.py
+++ app/app.py
@@ -2,18 +2,28 @@ import string
 import flask
 import pymongo
 import random
-import redis
 import flask_caching
 import flask_limiter
 
-REDIS_HOST = "redis"
-REDIS_PORT = 6379
-
 app = flask.Flask(__name__)
+
+
+def get_config() -> dict:
+    with app.app_context():
+        config = getattr(flask.g, "_config", None)
+
+        if config is None:
+            from dotenv import dotenv_values
+
+            config = dotenv_values(".env")
+
+        return config
+
+
 app.config.from_mapping(
     {
-        "CACHE_TYPE": "RedisCache",
-        "CACHE_REDIS_HOST": redis.Redis(host=REDIS_HOST, port=REDIS_PORT),
+        "CACHE_TYPE": "FileSystemCache",
+        "CACHE_DIR": "./cache/",
         "CACHE_DEFAULT_TIMEOUT": 60 * 60 * 24 * 30  # 1 month
     }
 )
@@ -24,7 +34,7 @@ limiter = flask_limiter.Limiter(
     app=app,
     default_limits=["10 per second", "100 per minute", "250 per hour", "500 per day"],
     # Redis
-    storage_uri=f"redis://{REDIS_HOST}:{REDIS_PORT}",
+    storage_uri=get_config()["MONGO_URL"],
     strategy="fixed-window",  # or "moving-window"
 )
 
@@ -52,17 +62,6 @@ def resource_not_found(e):
 @app.errorhandler(429)
 def resource_not_found(e):
     return flask.jsonify(error="429 Too Many Requests"), 429
-
-
-def get_config() -> dict:
-    config = getattr(flask.g, "_config", None)
-
-    if config is None:
-        from dotenv import dotenv_values
-
-        config = dotenv_values(".env")
-
-    return config
 
 
 @app.route("/", methods=["GET"])
